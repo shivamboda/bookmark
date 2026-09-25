3e0e690dfccc75ab61ecf5a40c6267f6f951b58c
Shivam Boda <shivamboda@gmail.com>
2026-09-25 10:17:34 +0530

    build(web): compile production release for GitHub Pages with 5-part fixes and iOS watchdog

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"af7e796e161ae0bb1ff0758c71a7105418bd9d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3922569589" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3181098687" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
